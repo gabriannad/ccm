60a8db3916d51e17aafe9c253fde4a1d28183cec
gabriannad <gabrianna@berkeley.edu>
2015-01-08 23:07:45 -0800

    fixed json

--- js/app.js
+++ js/app.js
@@ -3,74 +3,11 @@
 
 	app.controller('eventsController', ['$http', function($http){
 		var events = this;
-		store.products = []
+		events.eventList = []
 
 		$http.get('events.json').success(function(data){
-			events.days = eventList;
+			events.eventList = data;
 		})
 	}]);
-
-	var eventList = [
-		{
-			date: '2014-09-13',
-			events: [
-				{
-					eventType: 'Jam Session',
-					time: '12 - 2 PM',
-					location: 'morrison-hall',
-				},
-				{
-					eventType: 'Concert',
-					time: '2:30 - 3:30 PM',
-					location: 'kyakameena',
-				}
-			]
-		},
-		{
-			date: '2014-09-20',
-			events: [
-				{
-					eventType: 'Jam Session',
-					time: '12 - 2 PM',
-					location: 'morrison-hall',
-				},
-				{
-					eventType: 'Concert',
-					time: '2:30 - 3:30 PM',
-					location: 'elmwood',
-				}
-			]
-		},
-		{
-			date: '2014-09-27',
-			events: [
-				{
-					eventType: 'Jam Session',
-					time: '12 - 2 PM',
-					location: 'morrison-hall',
-				},
-				{
-					eventType: 'Concert',
-					time: '2:30 - 3:30 PM',
-					location: 'ashby',
-				}
-			]
-		},
-		{
-			date: '2014-10-04',
-			events: [
-				{
-					eventType: 'Jam Session',
-					time: '12 - 2 PM',
-					location: 'morrison-hall',
-				},
-				{
-					eventType: 'Concert',
-					time: '2:30 - 3:30 PM',
-					location: 'chaparral-house',
-				}
-			]
-		}
-	]
 
 })();
