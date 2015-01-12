04ffaa88518946f128fd9208721b339db8e50fa7
gabriannad <gabrianna@berkeley.edu>
2015-01-12 12:59:04 -0800

    remove document ready resize from bg.js

--- js/bg.js
+++ js/bg.js
@@ -1,11 +1,4 @@
 if( !(/iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
-    $(document).ready(function(){
-        if ($(window).width() > 584) {
-            $('#bg').width($(window).width() - 240);
-        } else {
-            $('#bg').width('100%');
-        }
-    });
 
     $(window).resize(function(){
         if ($(window).width() > 584) {
