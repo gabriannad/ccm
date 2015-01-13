c74ad206186f08b86f7e96f7446cfc3420c9d877
gabriannad <gabrianna@berkeley.edu>
2015-01-12 18:01:26 -0800

    used bg.js again

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,14 @@
 if( !(/iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
 
+    $(document).ready(function(){
+        if ($(window).width() > 584) {
+            $('#bg').width($(window).width() - 240);
+        } else {
+            $('#bg').width('100%');
+        }
+    });
+
+
     $(window).resize(function(){
         if ($(window).width() > 584) {
             $('#bg').width($(window).width() - 240);
